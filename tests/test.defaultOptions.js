34e63b21e9e0a56a17642e8256bf84175fe28985
Taylor Hanayik <hanayik@gmail.com>
2021-11-26 13:13:22 +0000

    add src documentation for public methods

--- tests/test.defaultOptions.js
+++ tests/test.defaultOptions.js
@@ -7,18 +7,6 @@ test('defaultOptions', async () => {
   let opts = await page.evaluate(async () => {
     let nv = new niivue.Niivue()
     await nv.attachTo('gl')
-    // load one volume object in an array
-    // var volumeList = [
-    //   {
-    //     url: "../images/mni152.nii.gz",
-    //     volume: { hdr: null, img: null },
-    //     name: "mni152",
-    //     colorMap: "gray",
-    //     opacity: 1,
-    //     visible: true,
-    //   },
-    // ]
-    // await nv.loadVolumes(volumeList)
     return nv.opts
   })
 
@@ -29,6 +17,4 @@ test('defaultOptions', async () => {
   expect(opts.crosshairColor).toEqual([1, 0, 0, 1])
   expect(opts.selectionBoxColor).toEqual([1, 1, 1, .5])
   expect(opts.colorBarMargin).toEqual(0.05)
-  expect(opts.briStep).toEqual(1) // deprecated since selection box is used now
-  expect(opts.conStep).toEqual(1) //deprecated since selection box is used now
 })
