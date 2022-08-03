a29d11b3a0316e3856b4d7bbb53f8e6aa8fe66fe
Chris Drake <christopher.a.drake@gmail.com>
2022-08-03 10:56:56 -0400

    Added timeseries control

--- src/nvmessage.js
+++ src/nvmessage.js
@@ -5,10 +5,12 @@ export const ADD_VOLUME_URL = "add volume url";
 export const REMOVE_VOLUME_URL = "remove volume media";
 export const ADD_MESH_URL = "add mesh url";
 export const REMOVE_MESH_URL = "remove mesh media";
+export const SET_4D_VOL_INDEX = "set 4d vol index";
 
 /**
  * @class NVMessageUpdateData
  * @type NVMessageUpdateData
+ * @constructor
  * @param {number} azimuth
  * @param {number} elevation
  * @param {number[]} clipPlane
@@ -24,6 +26,20 @@ export function NVMesssageUpdateData(azimuth, elevation, clipPlane, zoom) {
 }
 
 /**
+ * @class NVMessageSet4DVolumeIndex
+ * @type NVMessageSet4DVolumeIndex
+ * @constructor
+ * @param {string} url
+ * @param {number} index
+ */
+export function NVMessageSet4DVolumeIndexData(url, index) {
+  return {
+    url,
+    index,
+  };
+}
+
+/**
  * @typedef { import('./nvimage').NVImageFromUrlOptions } NVImageFromUrlOptions
  */
 
@@ -34,7 +50,7 @@ export function NVMesssageUpdateData(azimuth, elevation, clipPlane, zoom) {
  * NVMessage can be used to synchronize a session actions
  * @constructor
  * @param {string} messageType
- * @param {(string|NVMesssageUpdateData|NVImageFromUrlOptions)} messageData
+ * @param {(string|NVMesssageUpdateData|NVImageFromUrlOptions|NVMessageSet4DVolumeIndex)} messageData
  * @param {string} sessionKey
  */
 export function NVMessage(messageType, messageData = "", sessionKey = "") {
@@ -51,6 +67,10 @@ export function NVMessage(messageType, messageData = "", sessionKey = "") {
       break;
     case REMOVE_VOLUME_URL:
       message.url = messageData;
+      break;
+    case SET_4D_VOL_INDEX:
+      message.url = messageData.url;
+      message.index = messageData.index;
       break;
   }
 
