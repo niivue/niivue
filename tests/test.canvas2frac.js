f71bba7eebb3a911b59a68473ea746055c39a2f3
Alexis Thual <alexisthual@gmail.com>
2023-02-14 15:07:16 +0100

    Prettify all the files

--- tests/test.canvas2frac.js
+++ tests/test.canvas2frac.js
@@ -1,21 +1,21 @@
-const { snapshot, httpServerAddress, seconds } = require("./helpers")
+const { snapshot, httpServerAddress, seconds } = require("./helpers");
 beforeEach(async () => {
-  await page.goto(httpServerAddress, {timeout:0})
-  await page.setViewport({width: 1440, height: 900, deviceScaleFactor: 1});
-})
-test('canvasPos2frac', async () => {
+  await page.goto(httpServerAddress, { timeout: 0 });
+  await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 1 });
+});
+test("canvasPos2frac", async () => {
   let frac = await page.evaluate(async () => {
     let opts = {
       textHeight: 0.05, // larger text
-      crosshairColor: [0, 0, 1, 1] // green
-    }
-    let nv = new niivue.Niivue(opts = opts)
-    await nv.attachTo('gl', false)
+      crosshairColor: [0, 0, 1, 1], // green
+    };
+    let nv = new niivue.Niivue((opts = opts));
+    await nv.attachTo("gl", false);
 
     // load one volume object in an array
     var volumeList = [
       {
-        url: "./images/mni152.nii.gz",//"./RAS.nii.gz", "./spm152.nii.gz",
+        url: "./images/mni152.nii.gz", //"./RAS.nii.gz", "./spm152.nii.gz",
         volume: { hdr: null, img: null },
         name: "mni152.nii.gz",
         intensityMin: 0, // not used yet
@@ -25,14 +25,14 @@ test('canvasPos2frac', async () => {
         opacity: 1,
         visible: true,
       },
-    ]
-    await nv.loadVolumes(volumeList)
-    let pos = [100, 200]
-    let frac = nv.canvasPos2frac(pos)
-    return frac
-  })
-  let expected = [ 0.4045893719806762, 0.5, 0.5 ]
-  for (let i=0; i<frac.length; i++){
-    expect(frac[i]).toBeCloseTo(expected[i])
+    ];
+    await nv.loadVolumes(volumeList);
+    let pos = [100, 200];
+    let frac = nv.canvasPos2frac(pos);
+    return frac;
+  });
+  let expected = [0.4045893719806762, 0.5, 0.5];
+  for (let i = 0; i < frac.length; i++) {
+    expect(frac[i]).toBeCloseTo(expected[i]);
   }
-})
+});
--- tests/test.defaultOptions.js
+++ tests/test.defaultOptions.js
@@ -1,20 +1,20 @@
-const { snapshot, httpServerAddress, seconds } = require("./helpers")
+const { snapshot, httpServerAddress, seconds } = require("./helpers");
 beforeEach(async () => {
-  await page.goto(httpServerAddress, {timeout:0})
-  await page.setViewport({width: 1440, height: 900, deviceScaleFactor: 1});
-})
-test('defaultOptions', async () => {
+  await page.goto(httpServerAddress, { timeout: 0 });
+  await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 1 });
+});
+test("defaultOptions", async () => {
   let opts = await page.evaluate(async () => {
-    let nv = new niivue.Niivue()
-    await nv.attachTo('gl', false)
-    return nv.opts
-  })
+    let nv = new niivue.Niivue();
+    await nv.attachTo("gl", false);
+    return nv.opts;
+  });
 
-  expect(opts.textHeight).toEqual(0.06)
-  expect(opts.colorbarHeight).toEqual(0.05)
-  expect(opts.crosshairWidth).toEqual(1)
-  expect(opts.backColor).toEqual([0, 0, 0, 1])
-  expect(opts.crosshairColor).toEqual([1, 0, 0, 1])
-  expect(opts.selectionBoxColor).toEqual([1, 1, 1, .5])
-  expect(opts.colorbarMargin).toEqual(0.05)
-})
+  expect(opts.textHeight).toEqual(0.06);
+  expect(opts.colorbarHeight).toEqual(0.05);
+  expect(opts.crosshairWidth).toEqual(1);
+  expect(opts.backColor).toEqual([0, 0, 0, 1]);
+  expect(opts.crosshairColor).toEqual([1, 0, 0, 1]);
+  expect(opts.selectionBoxColor).toEqual([1, 1, 1, 0.5]);
+  expect(opts.colorbarMargin).toEqual(0.05);
+});
