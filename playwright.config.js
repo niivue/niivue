25fce6d1be7920b1a79ff6c5c8bfad4e48d29713
Chris Drake <christopher.a.drake@gmail.com>
2023-11-03 13:20:34 -0400

    Updated playwright to just run firefox tests

--- playwright.config.js
+++ playwright.config.js
@@ -34,20 +34,20 @@ module.exports = defineConfig({
   testMatch: '*test*.spec.js',
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
