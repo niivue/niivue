ca7b8aa9880c1e921f1a31178fb5d748a08322ba
Chris Drake <christopher.a.drake@gmail.com>
2023-11-03 13:36:39 -0400

    Update playwright config to run webkit tests

--- playwright.config.js
+++ playwright.config.js
@@ -39,15 +39,15 @@ module.exports = defineConfig({
     //   use: { ...devices['Desktop Chrome'] },
     // },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
     // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
     // },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
