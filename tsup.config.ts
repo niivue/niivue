297f20d3d59da0dfb53904edbe6cf4848d4155e9
Jens Ochsenmeier <github@jens-ox.de>
2023-11-29 23:43:22 +0100

    produce modern ES Module

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,16 +4,13 @@ export default defineConfig({
   entry: ['src/niivue/index.js'],
   outDir: 'dist',
   target: 'es2020',
-  splitting: false,
   format: 'esm',
   sourcemap: true,
   clean: true,
   loader: {
     '.jpg': 'dataurl',
     '.png': 'dataurl'
-  },
-  // TODO: remove this once testing setup doesn't rely on one single JS file
-  noExternal: [/(.*)/]
+  }
   // TODO: enable this as soon as TS port is completed
   // dts: true
 })
