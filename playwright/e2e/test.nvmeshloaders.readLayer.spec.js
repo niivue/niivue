5ddd825c6b0288929aefa32429bebf93517aa6ca
Chris Drake <christopher.a.drake@gmail.com>
2023-12-13 09:50:55 -0500

    Added disable no-undef eslint directive

--- playwright/e2e/test.nvmeshloaders.readLayer.spec.js
+++ playwright/e2e/test.nvmeshloaders.readLayer.spec.js
@@ -7,40 +7,48 @@ test.beforeEach(async ({ page }, testInfo) => {
 })
 
 test('nvmeshloaders readLayer', async ({ page }) => {
-  const nlayers = await page.evaluate(async () => {    
+  const nlayers = await page.evaluate(async () => {
     // load one volume object in an array
+    // eslint-disable-next-line no-undef
     const nv = new Niivue({
       show3Dcrosshair: true,
       backColor: [1, 1, 1, 1],
       meshXRay: 0.3
-    });
-    nv.attachTo("gl");
-    nv.opts.isColorbar = true;
-  
+    })
+    nv.attachTo('gl')
+    nv.opts.isColorbar = true
+
     await nv.loadMeshes([
       {
-        url: "./images/BrainMesh_ICBM152.lh.mz3",
-        rgba255: [255, 255, 255, 255],
+        url: './images/BrainMesh_ICBM152.lh.mz3',
+        rgba255: [255, 255, 255, 255]
       },
-      { url: "./images/CIT168.mz3", rgba255: [0, 0, 255, 255] },
-    ]);
-    const layer =
-    {
-      url: "./images/BrainMesh_ICBM152.lh.motor.mz3",
+      { url: './images/CIT168.mz3', rgba255: [0, 0, 255, 255] }
+    ])
+    const layer = {
+      url: './images/BrainMesh_ICBM152.lh.motor.mz3',
       cal_min: 0.5,
       cal_max: 5.5,
       useNegativeCmap: true,
-      opacity: 0.7,
-    };
+      opacity: 0.7
+    }
     const response = await fetch(layer.url)
     if (!response.ok) {
-      throw Error(response.statusText);
+      throw Error(response.statusText)
     }
-    const buffer = await response.arrayBuffer();
-    niivue.NVMeshLoaders.readLayer(layer.url, buffer, nv.meshes[0], layer.opacity, 'gray', undefined, layer.useNegativeCmap);
-    nv.meshes[0].updateMesh(nv.gl);
-    nv.drawScene();
-    return nv.meshes[0].layers.length;
+    const buffer = await response.arrayBuffer()
+    niivue.NVMeshLoaders.readLayer(
+      layer.url,
+      buffer,
+      nv.meshes[0],
+      layer.opacity,
+      'gray',
+      undefined,
+      layer.useNegativeCmap
+    )
+    nv.meshes[0].updateMesh(nv.gl)
+    nv.drawScene()
+    return nv.meshes[0].layers.length
   })
   expect(nlayers).toBe(1)
   await expect(page).toHaveScreenshot({ timeout: 30000 })
