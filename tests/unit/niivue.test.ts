0515f329a58153aa3938ca86ededb8c3d4e7fb4d
Taylor Hanayik <hanayik@gmail.com>
2023-12-14 08:27:51 +0000

    Update test script in package.json and update vitest.config.ts with excludes

--- tests/unit/niivue.test.ts
+++ tests/unit/niivue.test.ts
@@ -1,16 +1,12 @@
 import { expect, test } from 'vitest'
-import { Niivue } from './index.js' // note the js extension
-
-test('NiiVue', () => {
-  expect(Niivue).toBeDefined()
-})
+import { Niivue } from '../../src/niivue/index.js' // note the js extension
 
 test('backColor defaults to black', () => {
   const nv = new Niivue()
   expect(nv.opts.backColor).toStrictEqual([0, 0, 0, 1])
 })
 
-test('crosshairColor', async () => {
+test('crosshairColor can be set', async () => {
   const nv = new Niivue()
   const green = [0, 1, 0, 1] // RGBA green
   await nv.setCrosshairColor(green)
