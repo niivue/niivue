6d9bb9c58895c70df21d324a854cea84f6386c37
Chris Drake <christopher.a.drake@gmail.com>
2024-01-10 17:07:43 -0500

    Removed console.log

--- tests/unit/nvdocument.colormap.test.ts
+++ tests/unit/nvdocument.colormap.test.ts
@@ -3,9 +3,7 @@ import { NVDocument, DocumentData } from '../../src/niivue/index.js' // note the
 import * as nvd from '../images/document/niivue.mesh-old-colorMap.json';
 
 test('nvdocument convert colorMap and colorMapNegative to colormap and colormapNegative', () => {
-  
   const doc = NVDocument.loadFromJSON(nvd as DocumentData)
-  console.log(doc)
   assert(doc.meshDataObjects)
   const colorMapIsInLayer = "colorMap" in doc.meshDataObjects[0].layers[0]
   const colorMapNegativeIsInLayer = "colorMapNegative" in doc.meshDataObjects[0].layers[0]
@@ -13,6 +11,5 @@ test('nvdocument convert colorMap and colorMapNegative to colormap and colormapN
   expect(doc.meshDataObjects[0].layers[0].colormapNegative).toEqual("winter")
   expect(colorMapIsInLayer).toBe(false)
   expect(colorMapNegativeIsInLayer).toBe(false)
-  
 })
 
