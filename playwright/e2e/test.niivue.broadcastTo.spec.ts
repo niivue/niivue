c92db6b073755c413f6161daa6b992b61b502eb4
Chris Drake <christopher.a.drake@gmail.com>
2024-07-02 23:35:52 -0400

    Add test to verify pan2Dxyzmm is not synced after syncing is turned off

--- playwright/e2e/test.niivue.broadcastTo.spec.ts
+++ playwright/e2e/test.niivue.broadcastTo.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import { vec4 } from 'gl-matrix'
 import { Niivue } from '../../dist/index.js'
 import { httpServerAddressSync } from './helpers.js'
 import { TEST_OPTIONS } from './test.types.js'
@@ -35,10 +36,11 @@ test('niivue broadcastTo', async ({ page }) => {
 
 test('niivue broadcastTo can be turned off', async ({ page }) => {
   const scenesHandle = await page.evaluateHandle(async (testOptions) => {
-    const nv1 = new Niivue(testOptions)
+    const panTestOptions = { ...testOptions, dragMode: 3 }
+    const nv1 = new Niivue(panTestOptions)
     await nv1.attachTo('gl1')
     nv1.attachTo('gl1')
-    const nv2 = new Niivue(testOptions)
+    const nv2 = new Niivue(panTestOptions)
     await nv2.attachTo('gl2')
     const volumeList = [
       {
@@ -54,15 +56,19 @@ test('niivue broadcastTo can be turned off', async ({ page }) => {
     await nv2.loadVolumes(volumeList)
     nv1.broadcastTo(nv2)
     nv1.broadcastTo(nv2, { '2d': false, '3d': false })
+    nv1.setPan2Dxyzmm([5, -4, 2, 1.5])
     return [nv1.scene, nv2.scene]
   }, TEST_OPTIONS)
 
   let scenes = await scenesHandle.jsonValue()
   expect(scenes[0].crosshairPos[0]).toBe(scenes[1].crosshairPos[0])
   expect(scenes[0].crosshairPos[1]).toBe(scenes[1].crosshairPos[1])
+  expect(scenes[0].pan2Dxyzmm).toEqual(expect.arrayContaining([...Array.from(scenes[1].pan2Dxyzmm)]))
   await page.mouse.click(100, 200)
   scenes = await scenesHandle.jsonValue()
+  expect(Object.values(scenes[0].pan2Dxyzmm)).toEqual(expect.arrayContaining([5, -4, 2, 1.5]))
   expect(scenes[0].crosshairPos[0]).not.toBe(scenes[1].crosshairPos[0])
   expect(scenes[0].crosshairPos[1]).not.toBe(scenes[1].crosshairPos[1])
+  expect(Object.values(scenes[1].pan2Dxyzmm)).toEqual(expect.arrayContaining([0, 0, 0, 1]))
   await scenesHandle.dispose()
 })
