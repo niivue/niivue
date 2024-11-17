169a78d54340877c4ff0e0032211cba34fb916e0
Taylor Hanayik <hanayik@gmail.com>
2024-11-17 16:01:16 +0000

    add max-params for future work

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc', 'prettier'],
   rules: {
+    'max-params': 'off',
     'no-unreachable': 'error',
     curly: ['error', 'all'],
     camelcase: 'off',
