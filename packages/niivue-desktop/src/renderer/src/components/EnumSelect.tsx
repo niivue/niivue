599d1f5d1eaf0ce486b7b529e5364cec41be311e
Chris Drake <christopher.a.drake@gmail.com>
2025-01-29 13:14:53 -0500

    Update EnumSelect to use the “Select” component from radix-ui/themes

--- packages/niivue-desktop/src/renderer/src/components/EnumSelect.tsx
+++ packages/niivue-desktop/src/renderer/src/components/EnumSelect.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import * as Select from '@radix-ui/react-select'
+import { Select } from '@radix-ui/themes'
 
 export const EnumSelect: React.FC<{
   value: string
   onChange: (value: string) => void
   options: Record<string, number>
 }> = ({ value, onChange, options }) => (
-  <Select.Root value={value} onValueChange={onChange}>
-    <Select.Trigger className="flex items-center justify-between border rounded px-2 py-1">
-      <Select.Value />
-      <Select.Icon className="ml-2">▼</Select.Icon>
-    </Select.Trigger>
-    <Select.Content className="bg-white border rounded shadow">
-      <Select.Viewport>
-        {Object.entries(options).map(([label, val]) => (
-          <Select.Item key={val} value={val.toString()} className="p-2 hover:bg-gray-100">
-            <Select.ItemText>{label}</Select.ItemText>
-          </Select.Item>
-        ))}
-      </Select.Viewport>
-    </Select.Content>
-  </Select.Root>
+  <div className="flex flex-col gap-2 width-[200px] min-w-[200px]">
+    <div className="flex gap-1 justify-between items-center">
+      <Select.Root value={value} onValueChange={onChange}>
+        <Select.Trigger className="flex items-center justify-between border rounded px-2 py-1"></Select.Trigger>
+        <Select.Content className="truncate">
+          {Object.entries(options).map(([label, val]) => (
+            <Select.Item key={val} value={val.toString()}>
+              <span>{label}</span>
+            </Select.Item>
+          ))}
+        </Select.Content>
+      </Select.Root>
+    </div>
+  </div>
 )
