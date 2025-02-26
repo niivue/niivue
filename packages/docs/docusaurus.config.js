d6b3ae97af06fea2e8571c45374dc186b37a90a4
Taylor Hanayik <hanayik@gmail.com>
2025-02-26 16:22:21 +0100

    disable faster docs build

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -4,60 +4,60 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  future: {
-    experimental_faster: true,
-  },
-  title: 'NiiVue',
-  tagline: 'A WebGL2 medical image viewer',
-  favicon: 'img/favicon.ico',
+  // future: {
+  //   experimental_faster: true,
+  // },
+  title: "NiiVue",
+  tagline: "A WebGL2 medical image viewer",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://niivue.github.io',
+  url: "https://niivue.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/niivue/docusaurus/',
+  baseUrl: "/niivue/docusaurus/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'niivue', // Usually your GitHub org/user name.
-  projectName: 'niivue', // Usually your repo name.
+  organizationName: "niivue", // Usually your GitHub org/user name.
+  projectName: "niivue", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
-  plugins: [require.resolve('./niivuePlugin')],
+  plugins: [require.resolve("./niivuePlugin")],
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
-            // 'https://github.com/niivue/niivue'
+          // 'https://github.com/niivue/niivue'
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
-            // 'https://github.com/niivue/niivue'
+          // 'https://github.com/niivue/niivue'
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -69,57 +69,57 @@ const config = {
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'NiiVue',
+        title: "NiiVue",
         // logo: {
         //   alt: 'My Site Logo',
         //   src: 'img/logo.svg',
         // },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Getting started',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Getting started",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/niivue/niivue',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/niivue/niivue",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Issues',
-                href: 'https://github.com/niivue/niivue/issues',
+                label: "Issues",
+                href: "https://github.com/niivue/niivue/issues",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/niivue/niivue',
+                label: "GitHub",
+                href: "https://github.com/niivue/niivue",
               },
             ],
           },
