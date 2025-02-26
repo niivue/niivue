4fa0a99e6ad289b90bf799f7d00b6ce329da1cfc
Taylor Hanayik <hanayik@gmail.com>
2025-02-26 11:55:52 +0100

    fix unit tests

--- packages/niivue/tests/unit/nvimage.test.ts
+++ packages/niivue/tests/unit/nvimage.test.ts
@@ -1,40 +1,35 @@
-import { expect, test } from "vitest"
-import { NVImage } from "../../src/niivue/index.js" // note the js extension
+import { expect, test } from 'vitest'
+import { NVImage } from '../../src/niivue/index.js' // note the js extension
 import { readFileSync } from 'node:fs'
 import path from 'path'
 import { vec3 } from 'gl-matrix'
 
-test("nvimage convertVox2Frac", () => {
-    const name = "mni152.nii.gz"
-    const dataBuffer = readFileSync(path.join("./tests/images/", name))
-    const image = new NVImage(
-        dataBuffer.buffer,
-        name)
-    const vox =vec3.fromValues(103, 128, 129)
-    const frac = image.convertVox2Frac(vox)
-    const expected = [0.5000415009576917, 0.5017796754837036, 0.6023715706758721]
-    for (let i = 0; i < frac.length; i++) {
-        expect(frac[i]).toBeCloseTo(expected[i])
-    }
+test('nvimage convertVox2Frac', async () => {
+  const name = 'mni152.nii.gz'
+  const dataBuffer = readFileSync(path.join('./tests/images/', name))
+  const image = await NVImage.new(dataBuffer.buffer, name)
+  const vox = vec3.fromValues(103, 128, 129)
+  const frac = image.convertVox2Frac(vox)
+  const expected = [0.5000415009576917, 0.5017796754837036, 0.6023715706758721]
+  for (let i = 0; i < frac.length; i++) {
+    expect(frac[i]).toBeCloseTo(expected[i])
+  }
 })
 
-test("nvimage zerosLike", () => {
-    const name = "mni152.nii.gz"
-    const dataBuffer = readFileSync(path.join("./tests/images/", name))
-    const image = new NVImage(
-        dataBuffer.buffer,
-        name)
-    const zeroImage = NVImage.zerosLike(image)
-    expect(JSON.stringify(image.hdr)).toBe(JSON.stringify(zeroImage.hdr))
-    expect(zeroImage.img!.every((item: number) => item === 0)).toBeTruthy()
+test('nvimage zerosLike', async () => {
+  const name = 'mni152.nii.gz'
+  const dataBuffer = readFileSync(path.join('./tests/images/', name))
+  const image = await NVImage.new(dataBuffer.buffer, name)
+  const zeroImage = NVImage.zerosLike(image)
+  expect(JSON.stringify(image.hdr)).toBe(JSON.stringify(zeroImage.hdr))
+  expect(zeroImage.img!.every((item: number) => item === 0)).toBeTruthy()
 })
 
-test("nvimage clone has unique ID", () => {
-    const name = "mni152.nii.gz"
-    const dataBuffer = readFileSync(path.join("./tests/images/", name))
-    const image = new NVImage(
-        dataBuffer.buffer,
-        name)
-    const clone = image.clone()
-    expect(clone.id).not.toBe(image.id)
+test('nvimage clone has unique ID', async () => {
+  const name = 'mni152.nii.gz'
+  const dataBuffer = readFileSync(path.join('./tests/images/', name))
+  const image = await NVImage.new(dataBuffer.buffer, name)
+  const clone = image.clone()
+  expect(clone.id).not.toBe(image.id)
 })
+
--- packages/niivue/tests/unit/nvmesh.test.ts
+++ packages/niivue/tests/unit/nvmesh.test.ts
@@ -1,7 +1,7 @@
-import { expect, test } from "vitest";
-import { NVMesh } from "../../src/niivue/index.js"; // note the js extension
+import { expect, test } from 'vitest'
+import { NVMeshLoaders } from '../../src/niivue/index.js' // note the js extension
 
-function base64ToArrayBuffer(base64) {
+function base64ToArrayBuffer(base64: string) {
   const binary_string = atob(base64)
   const len = binary_string.length
   const bytes = new Uint8Array(len)
@@ -11,7 +11,7 @@ function base64ToArrayBuffer(base64) {
   return bytes.buffer
 }
 
-test("nvmesh parses xml gifti file with new line", () => {
+test('nvmesh parses xml gifti file with new line', async () => {
   const xmlFile = `
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE GIFTI SYSTEM "http://www.nitrc.org/frs/download.php/115/gifti.dtd">
@@ -28,20 +28,20 @@ test("nvmesh parses xml gifti file with new line", () => {
   0.000000   0.000000   0.000000   1.000000</MatrixData>
 </CoordinateSystemTransformMatrix>
 <Data>eJxjYGiwZ2BgcAAiBwAJQwGA</Data>
-</DataArray></GIFTI>`;
-const buffer = base64ToArrayBuffer(btoa(xmlFile))
-const obj = NVMesh.readGII(buffer)
-expect(Object.values(obj.scalars).length).toBe(3)
-});
+</DataArray></GIFTI>`
+  const buffer = base64ToArrayBuffer(btoa(xmlFile))
+  const obj = await NVMeshLoaders.readGII(buffer)
+  expect(Object.values(obj.scalars).length).toBe(3)
+})
 
-test("nvmesh parses xml gifti file with no new line", () => {
+test('nvmesh parses xml gifti file with no new line', async () => {
   const xmlFile = `
   <?xml version="1.0" encoding="UTF-8"?><!DOCTYPE GIFTI SYSTEM "http://www.nitrc.org/frs/download.php/115/gifti.dtd"><GIFTI Version="1.0" NumberOfDataArrays="1"><MetaData /><LabelTable /><DataArray Intent="NIFTI_INTENT_ZSCORE" DataType="NIFTI_TYPE_FLOAT32" ArrayIndexingOrder="RowMajorOrder" Dimensionality="1" Encoding="GZipBase64Binary" Endian="LittleEndian" ExternalFileName="" ExternalFileOffset="0" Dim0="3"><MetaData /><CoordinateSystemTransformMatrix><DataSpace>NIFTI_XFORM_UNKNOWN</DataSpace><TransformedSpace>NIFTI_XFORM_UNKNOWN</TransformedSpace>
   <MatrixData>  1.000000   0.000000   0.000000   0.000000
     0.000000   1.000000   0.000000   0.000000
     0.000000   0.000000   1.000000   0.000000
     0.000000   0.000000   0.000000   1.000000</MatrixData></CoordinateSystemTransformMatrix><Data>eJxjYGiwZ2BgcAAiBwAJQwGA</Data></DataArray></GIFTI>`
-const buffer = base64ToArrayBuffer(btoa(xmlFile))
-const obj = NVMesh.readGII(buffer)
-expect(Object.values(obj.scalars).length).toBe(3)
-});
+  const buffer = base64ToArrayBuffer(btoa(xmlFile))
+  const obj = await NVMeshLoaders.readGII(buffer)
+  expect(Object.values(obj.scalars).length).toBe(3)
+})
