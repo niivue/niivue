bc76c6d1b61c0f8d92c5aae642ffd1323db99160
neurolabusc <rorden@sc.edu>
2025-03-07 09:22:19 -0500

    Limit canvas resolution (https://github.com/niivue/niivue/issues/1187)

--- packages/niivue/src/nvdocument.ts
+++ packages/niivue/src/nvdocument.ts
@@ -137,7 +137,7 @@ export type NVConfigOptions = {
   isSliceMM: boolean
   // V1 image overlays can show vectors as per-pixel lines
   isV1SliceShader: boolean
-  isHighResolutionCapable: boolean
+  forceDevicePixelRatio: number
   logLevel: 'debug' | 'info' | 'warn' | 'error' | 'fatal' | 'silent'
   loadingText: string
   isForceMouseClickToVoxelCenters: boolean
@@ -241,7 +241,7 @@ export const DEFAULT_OPTIONS: NVConfigOptions = {
   sagittalNoseLeft: false,
   isSliceMM: false,
   isV1SliceShader: false,
-  isHighResolutionCapable: true,
+  forceDevicePixelRatio: 0,
   logLevel: 'info',
   loadingText: 'loading ...',
   isForceMouseClickToVoxelCenters: false,
