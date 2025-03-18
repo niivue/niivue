d0cce2f2e79454ad1635aee984127a5c3d815a48
Chris Drake <christopher.a.drake@gmail.com>
2025-03-18 13:40:55 -0400

    added await for loadFromBase64

--- packages/niivue-desktop/src/renderer/src/utils/baseName.ts
+++ packages/niivue-desktop/src/renderer/src/utils/baseName.ts
@@ -1,4 +1,7 @@
 export const baseName = (path: string): string => {
+  if (!path) {
+    return ''
+  }
   const pathSep = path.includes('/') ? '/' : '\\'
   const parts = path.split(pathSep)
   return parts[parts.length - 1]
