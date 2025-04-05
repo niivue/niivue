7213fd1d1329c567e0be3261b72462f8bb5ce3e0
Chris Drake <christopher.a.drake@gmail.com>
2025-04-05 12:55:37 -0400

    Split up slow tests into separate files

---
+++ packages/niivue/playwright/e2e/test.niivue.loadDocumentFromUrl.replaces.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+import { Niivue } from '../../dist/index.js'
+import { httpServerAddress } from './helpers.js'
+import { TEST_OPTIONS } from './test.types.js'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(httpServerAddress)
+})
+
+test('niivue loadDocumentFromUrl replaces previous document objects', async ({ page }) => {
+  const { nvols, nmeshes } = await page.evaluate(async (testOptions) => {
+    const nv = new Niivue(testOptions)
+    await nv.attachTo('gl')
+    await nv.loadDocumentFromUrl('./images/document/niivue.basic.nvd')
+    await nv.loadDocumentFromUrl('./images/document/niivue.mesh-pre-0.52.0.nvd')
+    return { nvols: nv.volumes.length, nmeshes: nv.meshes.length }
+  }, TEST_OPTIONS)
+  expect(nvols).toBe(0)
+  expect(nmeshes).toBe(1)
+  await expect(page).toHaveScreenshot({ timeout: 30000 })
+})
