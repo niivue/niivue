7d13f85954ebc26cad6383d1217d79b652ee674a
Chris Drake <christopher.a.drake@gmail.com>
2025-04-05 12:22:36 -0400

    reenable loadDocumentFromUrl tests, removed waiting for page timeout calls.

--- packages/niivue/playwright/e2e/test.niivue.loadDocumentFromUrl.spec.ts
+++ packages/niivue/playwright/e2e/test.niivue.loadDocumentFromUrl.spec.ts
@@ -7,7 +7,7 @@ test.beforeEach(async ({ page }) => {
   await page.goto(httpServerAddress)
 })
 
-test.skip('niivue loadDocumentFromUrl nifti volume', async ({ page }) => {
+test('niivue loadDocumentFromUrl nifti volume', async ({ page }) => {
   const nvols = await page.evaluate(async (testOptions) => {
     const nv = new Niivue(testOptions)
     await nv.attachTo('gl')
@@ -15,11 +15,10 @@ test.skip('niivue loadDocumentFromUrl nifti volume', async ({ page }) => {
     return nv.volumes.length
   }, TEST_OPTIONS)
   expect(nvols).toBe(2)
-  await page.waitForTimeout(1000)
   await expect(page).toHaveScreenshot({ timeout: 30000 })
 })
 
-test.skip('niivue loadDocumentFromUrl nifti volume drawing', async ({ page }) => {
+test('niivue loadDocumentFromUrl nifti volume drawing', async ({ page }) => {
   const isDrawingPresent = await page.evaluate(async (testOptions) => {
     const nv = new Niivue(testOptions)
     await nv.attachTo('gl')
@@ -27,11 +26,10 @@ test.skip('niivue loadDocumentFromUrl nifti volume drawing', async ({ page }) =>
     return nv.drawBitmap != null
   }, TEST_OPTIONS)
   expect(isDrawingPresent).toBe(true)
-  await page.waitForTimeout(1000)
   await expect(page).toHaveScreenshot({ timeout: 30000 })
 })
 
-test.skip('niivue loadDocumentFromUrl mesh', async ({ page }) => {
+test('niivue loadDocumentFromUrl mesh', async ({ page }) => {
   const counts = await page.evaluate(async (testOptions) => {
     // eslint-disable-next-line no-undef
     const nv = new Niivue(testOptions)
@@ -44,11 +42,10 @@ test.skip('niivue loadDocumentFromUrl mesh', async ({ page }) => {
   }, TEST_OPTIONS)
   expect(counts.meshCount).toBe(1)
   expect(counts.layerCount).toBe(1)
-  await page.waitForTimeout(1000)
   await expect(page).toHaveScreenshot({ timeout: 30000 })
 })
 
-test.skip('niivue loadDocumentFromUrl replaces previous document objects', async ({ page }) => {
+test('niivue loadDocumentFromUrl replaces previous document objects', async ({ page }) => {
   const { nvols, nmeshes } = await page.evaluate(async (testOptions) => {
     // eslint-disable-next-line no-undef
     const nv = new Niivue(testOptions)
@@ -60,11 +57,10 @@ test.skip('niivue loadDocumentFromUrl replaces previous document objects', async
   }, TEST_OPTIONS)
   expect(nvols).toBe(0)
   expect(nmeshes).toBe(1)
-  await page.waitForTimeout(1000)
   await expect(page).toHaveScreenshot({ timeout: 30000 })
 })
 
-test.skip('niivue loadDocumentFromUrl compound document gzipped', async ({ page }) => {
+test('niivue loadDocumentFromUrl compound document gzipped', async ({ page }) => {
   const nvols = await page.evaluate(async (testOptions) => {
     const nv = new Niivue(testOptions)
     await nv.attachTo('gl')
@@ -72,6 +68,5 @@ test.skip('niivue loadDocumentFromUrl compound document gzipped', async ({ page 
     return nv.volumes.length
   }, TEST_OPTIONS)
   expect(nvols).toBe(2)
-  await page.waitForTimeout(1000)
   await expect(page).toHaveScreenshot({ timeout: 30000 })
 })
