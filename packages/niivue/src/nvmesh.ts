d651150cb5dfc4a1704e1071a84945b29f284a80
neurolabusc <rorden@sc.edu>
2025-04-20 09:02:05 -0400

    Kludge for mesh property check (https://github.com/niivue/niivue/issues/1256)

--- packages/niivue/src/nvmesh.ts
+++ packages/niivue/src/nvmesh.ts
@@ -1540,10 +1540,10 @@ export class NVMesh {
       console.warn('Mesh does not have property:', key, this)
       return
     }
-    if (typeof val !== 'number' && typeof val !== 'string' && typeof val !== 'boolean' && !Array.isArray(val)) {
+    /* if (typeof val !== 'number' && typeof val !== 'string' && typeof val !== 'boolean' && !Array.isArray(val)) {
       console.warn('Invalid value type. Expected number, numbers, string, or boolean but received:', typeof val)
       return
-    }
+    } */
     ;(this as any)[key] = val // TypeScript safety workaround
     this.updateMesh(gl) // Apply the new properties
   }
