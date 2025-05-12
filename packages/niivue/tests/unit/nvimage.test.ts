0e19db4c63b3c97ef812726e39ae0b9d4f8da652
Taylor Hanayik <hanayik@gmail.com>
2025-05-12 11:23:04 +0100

    fix draw textture 2D vs 3D switching; update testing

--- packages/niivue/tests/unit/nvimage.test.ts
+++ packages/niivue/tests/unit/nvimage.test.ts
@@ -33,15 +33,3 @@ test('nvimage clone has unique ID', async () => {
   const clone = image.clone()
   expect(clone.id).not.toBe(image.id)
 })
-
-test('nvimage getValue', async () => {
-  const name = 'hippo.nii.gz'
-  const dataBuffer = readFileSync(path.join('./tests/images/', name))
-  const image = await NVImage.new(dataBuffer.buffer, name)
-  const voxX = 31
-  const voxY = 54
-  const voxZ = 27
-  const value = image.getValue(voxX, voxY, voxZ)
-  // The expected value at this position should be 9
-  expect(value).toBe(9)
-})
--- packages/niivue/vitest.config.ts
+++ packages/niivue/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, coverageConfigDefaults } from 'vitest/config'
 export default defineConfig({
   test: {
     fileParallelism: false,
+    maxConcurrency: 1,
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
