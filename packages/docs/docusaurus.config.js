7943da65be8eb217781d0cbefa2d88ff9ba72ed6
Taylor Hanayik <hanayik@gmail.com>
2025-06-01 17:57:30 +0100

    update site URL and base path in Docusaurus config

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -16,10 +16,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://niivue.github.io",
+  url: "https://niivue.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/niivue/docusaurus/",
+  baseUrl: "",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
