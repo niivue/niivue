3fcdac918bf572e1710f240520c791ca10a62572
Chris Drake <christopher.a.drake@gmail.com>
2025-06-05 15:08:47 -0400

    add expect for gzip magic getValue test for test runner

--- packages/niivue/tests/unit/getValue.test.ts
+++ packages/niivue/tests/unit/getValue.test.ts
@@ -10,6 +10,13 @@ test('nvimage getValue', async () => {
   if (!dataBuffer.length) {
     throw new Error('buffer not loaded')
   }
+  expect(dataBuffer.length).toBeGreaterThan(0)
+  const u8 = new Uint8Array(dataBuffer.buffer, dataBuffer.byteOffset, dataBuffer.byteLength)
+  expect(u8[0]).toBe(0x1f) // first magic byte for gzip
+  expect(u8[1]).toBe(0x8b) // second magic byte for gzip
+  // 3) OPTIONAL debug: first 4 bytes should be gzip magic 0x1f8b0800
+  // console.log('First 4 bytes of hippo.nii.gz (hex):', dataBuffer.subarray(0, 4).toString('hex'))
+
   // console.log('data', dataBuffer)
   const image = await NVImage.new(dataBuffer.buffer, name)
   // console.log('image', image)
