6ce6c736784c6598a31aa735ff96b4e667d6f300
leej3 <johnleenimh@gmail.com>
2025-06-18 17:45:16 +0100

    update circle glsl

--- packages/uikit/src/shaders/frag/circle.frag.glsl
+++ packages/uikit/src/shaders/frag/circle.frag.glsl
@@ -9,11 +9,21 @@ in vec2 vUV;
 out vec4 color;
 
 void main() {
-  // Check if the pixel is inside the circle and color it
-  float distance = length(vUV - vec2(0.5, 0.5));
-  if (distance < 0.5 && distance >= (1.0 - fillPercent) / 2.0) {
-    color = circleColor;//vec4(circleColor.r, circleColor.g, circleColor.b, circleColor.a);
-  } else {
-    discard;
-  }
+float radius = 0.5;
+float innerRadius = (1.0 - fillPercent) / 2.0;
+vec2 center = vec2(0.5, 0.5);
+float dist = length(vUV - center);
+float pixelWidth = fwidth(dist);
+
+float alphaOuter = smoothstep(radius, radius - pixelWidth, dist);
+float alpha = alphaOuter;
+
+if (fillPercent < 1.0) {
+float alphaInner = smoothstep(innerRadius, innerRadius + pixelWidth, dist);
+alpha *= alphaInner;
+}
+
+if (alpha <= 0.0) discard;
+color = vec4(circleColor.rgb, circleColor.a * alpha);
+color.r = 0.0;
 }
