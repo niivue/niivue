05393e6749552dbc84c7700d417f305086b85783
Taylor Hanayik <hanayik@gmail.com>
2025-08-01 12:16:08 +0100

    update demo and add new tests for orientation markers

--- packages/niivue/playwright/e2e/test.niivue.setIsOrientationTextVisible.spec.ts
+++ packages/niivue/playwright/e2e/test.niivue.setIsOrientationTextVisible.spec.ts
@@ -31,3 +31,157 @@ test('niivue setIsOrientationTextVisible', async ({ page }) => {
   await page.waitForTimeout(1000)
   await expect(page).toHaveScreenshot({ timeout: 30000 })
 })
+
+test('niivue showAllOrientationMarkers default', async ({ page }) => {
+  const nvols = await page.evaluate(async (testOptions) => {
+    // eslint-disable-next-line no-undef
+    const nv = new Niivue(testOptions)
+    await nv.attachTo('gl')
+    const volumeList = [
+      {
+        url: './images/mni152.nii.gz',
+        volume: { hdr: null, img: null },
+        name: 'mni152.nii.gz',
+        colormap: 'gray',
+        opacity: 1,
+        visible: true
+      }
+    ]
+    await nv.loadVolumes(volumeList)
+    nv.setSliceType(nv.sliceTypeMultiplanar)
+    nv.setShowAllOrientationMarkers(false) // default - show only 2 markers
+    return nv.volumes.length
+  }, TEST_OPTIONS)
+  expect(nvols).toBe(1)
+  await page.waitForTimeout(1000)
+  await expect(page).toHaveScreenshot({ timeout: 30000 })
+})
+
+test('niivue showAllOrientationMarkers true', async ({ page }) => {
+  const nvols = await page.evaluate(async (testOptions) => {
+    // eslint-disable-next-line no-undef
+    const nv = new Niivue(testOptions)
+    await nv.attachTo('gl')
+    const volumeList = [
+      {
+        url: './images/mni152.nii.gz',
+        volume: { hdr: null, img: null },
+        name: 'mni152.nii.gz',
+        colormap: 'gray',
+        opacity: 1,
+        visible: true
+      }
+    ]
+    await nv.loadVolumes(volumeList)
+    nv.setSliceType(nv.sliceTypeMultiplanar)
+    nv.setShowAllOrientationMarkers(true) // show all 4 markers
+    return nv.volumes.length
+  }, TEST_OPTIONS)
+  expect(nvols).toBe(1)
+  await page.waitForTimeout(1000)
+  await expect(page).toHaveScreenshot({ timeout: 30000 })
+})
+
+test('niivue isRadiological false with default markers', async ({ page }) => {
+  const nvols = await page.evaluate(async (testOptions) => {
+    // eslint-disable-next-line no-undef
+    const nv = new Niivue(testOptions)
+    await nv.attachTo('gl')
+    const volumeList = [
+      {
+        url: './images/mni152.nii.gz',
+        volume: { hdr: null, img: null },
+        name: 'mni152.nii.gz',
+        colormap: 'gray',
+        opacity: 1,
+        visible: true
+      }
+    ]
+    await nv.loadVolumes(volumeList)
+    nv.setSliceType(nv.sliceTypeMultiplanar)
+    nv.setRadiologicalConvention(false) // neurological
+    nv.setShowAllOrientationMarkers(false)
+    return nv.volumes.length
+  }, TEST_OPTIONS)
+  expect(nvols).toBe(1)
+  await page.waitForTimeout(1000)
+  await expect(page).toHaveScreenshot({ timeout: 30000 })
+})
+
+test('niivue isRadiological true with default markers', async ({ page }) => {
+  const nvols = await page.evaluate(async (testOptions) => {
+    // eslint-disable-next-line no-undef
+    const nv = new Niivue(testOptions)
+    await nv.attachTo('gl')
+    const volumeList = [
+      {
+        url: './images/mni152.nii.gz',
+        volume: { hdr: null, img: null },
+        name: 'mni152.nii.gz',
+        colormap: 'gray',
+        opacity: 1,
+        visible: true
+      }
+    ]
+    await nv.loadVolumes(volumeList)
+    nv.setSliceType(nv.sliceTypeMultiplanar)
+    nv.setRadiologicalConvention(true) // radiological
+    nv.setShowAllOrientationMarkers(false)
+    return nv.volumes.length
+  }, TEST_OPTIONS)
+  expect(nvols).toBe(1)
+  await page.waitForTimeout(1000)
+  await expect(page).toHaveScreenshot({ timeout: 30000 })
+})
+
+test('niivue isRadiological false with all markers', async ({ page }) => {
+  const nvols = await page.evaluate(async (testOptions) => {
+    // eslint-disable-next-line no-undef
+    const nv = new Niivue(testOptions)
+    await nv.attachTo('gl')
+    const volumeList = [
+      {
+        url: './images/mni152.nii.gz',
+        volume: { hdr: null, img: null },
+        name: 'mni152.nii.gz',
+        colormap: 'gray',
+        opacity: 1,
+        visible: true
+      }
+    ]
+    await nv.loadVolumes(volumeList)
+    nv.setSliceType(nv.sliceTypeMultiplanar)
+    nv.setRadiologicalConvention(false) // neurological
+    nv.setShowAllOrientationMarkers(true) // all 4 markers
+    return nv.volumes.length
+  }, TEST_OPTIONS)
+  expect(nvols).toBe(1)
+  await page.waitForTimeout(1000)
+  await expect(page).toHaveScreenshot({ timeout: 30000 })
+})
+
+test('niivue isRadiological true with all markers', async ({ page }) => {
+  const nvols = await page.evaluate(async (testOptions) => {
+    // eslint-disable-next-line no-undef
+    const nv = new Niivue(testOptions)
+    await nv.attachTo('gl')
+    const volumeList = [
+      {
+        url: './images/mni152.nii.gz',
+        volume: { hdr: null, img: null },
+        name: 'mni152.nii.gz',
+        colormap: 'gray',
+        opacity: 1,
+        visible: true
+      }
+    ]
+    await nv.loadVolumes(volumeList)
+    nv.setSliceType(nv.sliceTypeMultiplanar)
+    nv.setRadiologicalConvention(true) // radiological
+    nv.setShowAllOrientationMarkers(true) // all 4 markers
+    return nv.volumes.length
+  }, TEST_OPTIONS)
+  expect(nvols).toBe(1)
+  await page.waitForTimeout(1000)
+  await expect(page).toHaveScreenshot({ timeout: 30000 })
+})
