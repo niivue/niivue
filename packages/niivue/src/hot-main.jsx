4cf50163ded98e7d6bf995dc3846fc26e4ef97cc
neurolabusc <rorden@sc.edu>
2026-02-24 12:36:34 -0500

    Remove hot-reloadable react demo

--- packages/niivue/src/hot-main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { HotGestureDemo } from './HotDemo.jsx'
-
-createRoot(document.getElementById('root')).render(<HotGestureDemo />)
